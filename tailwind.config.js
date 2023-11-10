c23723904b4f04639d055a5ac9a2af5373e9c576
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-11-10 14:55:25 +0100

    adding dist8

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,6 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    height: (theme) => ({
-      auto: "auto",
-      ...theme("spacing"),
-      full: "100%",
-      screen: "calc(var(--vh) * 100)",
-    }),
-    minHeight: (theme) => ({
-      0: "0",
-      ...theme("spacing"),
-      full: "100%",
-      screen: "calc(var(--vh) * 100)",
-    }),
     extend: {
       fontFamily: {
         almarai: "'Almarai', sans-serif",
